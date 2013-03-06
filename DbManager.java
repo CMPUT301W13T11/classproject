bb151145b7399cc3fac61664cf351d5571812ace
tupes <tupes23@gmail.com>
2013-03-06 01:27:03 -0700

    implements FModel
    decided not to extend SQLiteOpenHelper for now, i'll just add that
    in has-a relationship

---
+++ DbManager.java
@@ -0,0 +1,24 @@
+
+
+public class DbManager<V implements FView> implements FModel<V implements FView> {
+  
+  private Collection<V view> views;
+
+  // implement FModel methods
+  public void addView(V view) {
+    if (!views.contains(view)) {
+      views.add(view);
+    }
+  }
+
+  public void deleteView(V view) {
+    views.remove(view);
+  }
+
+  public void notifyViews() {
+    for (V view : views) {
+      view.update(this);
+    }
+  }
+
+}
