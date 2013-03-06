0eebf60a5b1e3526d3b32d1e5e019636ba915535
Michael Shirt <mshirt@ualberta.ca>
2013-03-05 19:30:57 -0700

    Testing Custom Buttons
    Trying to get the custom button style to work, it seems to at the
    moment.

--- FoodBook/gen/com/team11/foodbook/R.java
+++ FoodBook/gen/com/team11/foodbook/R.java
@@ -11,10 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button_style1=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main_screen=0x7f030000;
@@ -54,5 +56,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int Button1=0x7f050003;
+        public static final int styleName=0x7f050002;
     }
 }
