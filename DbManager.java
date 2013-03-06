4889c1cf35d41bb93edd497a2c267f0d3a4f006c
tupes <tupes23@gmail.com>
2013-03-06 02:04:58 -0700

    Update DbManager.java

--- DbManager.java
+++ DbManager.java
@@ -1,4 +1,3 @@
-
 public class DbManager<V implements FView> implements FModel<V implements FView> {
   
   private Collection<V> views;
@@ -18,6 +17,32 @@ public class DbManager<V implements FView> implements FModel<V implements FView>
     for (V view : views) {
       view.update(this);
     }
+  }
+
+  // for the controllers
+  public void save() {
+  }
+  
+  public void load() {
+    
+  }
+
+  public void insert(Recipe recipe) {
+    ContentValues values = RecipeToMap(recipe);
+    db.insert("UserRecipes", null, values);
+  }
+  
+  public void delete(Recipe recipe) {
+    db.delete("UserRecipes", "Name = " + recipe.getName()); // FIX!
+  }
+
+  public void insert(Ingredient ingred) {
+    ContentValues values = IngredientToMap(ingred);
+    db.insert("UserIngredients", null, values);
+  }
+  
+  public void delete(Ingredient ingred) {
+    db.delete("UserIngredients", "Name = " + ingred.getName()); // FIX!
   }
 
 }
